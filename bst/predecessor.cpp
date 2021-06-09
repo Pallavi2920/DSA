4c96275527b8f31706c5f439f9b93c8f9ee2b2ec
Pallavi2920 <pallavigupta2920@gmail.com>
2021-06-09 15:09:10 +0530

    second commit

--- bst/predecessor.cpp
+++ bst/predecessor.cpp
@@ -6,7 +6,7 @@ typedef struct Node
     int data;
     Node *left,*right;
 }Node;
-Node *root = NULL;
+
 Node *insert(Node *root,int key)
 {
     if(root == NULL)
@@ -27,20 +27,67 @@ Node *insert(Node *root,int key)
     }
     return root;
 }
-void addData(int key)
+void findNode(Node *node , Node *&pre,Node *&suc,int key)
 {
-    insert(root,key);
+    if(node==NULL)  return;
+    if(key==node->data)
+    {
+        if(node->left!=NULL)
+        {
+            Node *temp = node->left;
+            while(temp->right)
+            {
+                temp = temp->right;
+            }
+            pre = temp;
+        }
+        if(node->right!=NULL)
+        {
+            Node  *temp1 = node->right;
+            while(temp1->left)
+            {
+                temp1 = temp1->left;
+            }
+            suc = temp1;
+        }
+        return;
+    }
+    if(key<node->data)
+    {
+        suc = node;
+        findNode(node->left,pre,suc,key);
+    }
+    else
+    {
+        pre = node;
+        findNode(node->right,pre,suc,key);
+    }
 }
-
 int main()
 {
-    int n;
-    cout<<"Enter the number of elements you want to insert:";
-    cin>>n;
-    for(int i =0 ;i<n;i++)
-    {
-        int ele;
-        cin>>ele;
-        addData(ele);
-    }
+    Node *root = NULL;
+    root = insert(root , 50);
+    insert(root, 30);
+    insert(root, 20);
+    insert(root, 40);
+    insert(root, 70);
+    insert(root, 60);
+    insert(root, 80);
+    Node *pre = NULL,*suc = NULL;
+    int x;
+    cout<<"Enter the key for whom you want to search for predecessor and successor"<<endl;
+    cin>>x;
+    findNode(root,pre,suc,x);
+    if (pre != NULL)
+      cout << "Predecessor is " << pre->data << endl;
+    else
+      cout << "No Predecessor"<<endl;
+
+    if (suc != NULL)
+      cout << "Successor is " << suc->data;
+    else
+      cout << "No Successor"<<endl;
+
+
+    return 0;
 }
